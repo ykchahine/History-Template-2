28d9f976202834c8a735f960b082f73f44e666a6
Youssef Chahine <ykchahine@csu.fullerton.edu>
2021-11-30 14:10:11 -0800

    Added sound and input validation functions for Q4

--- js/history.js
+++ js/history.js
@@ -14,6 +14,21 @@ var trumpet_player = function() {
   trumpet.play();
 };
 
+var plane_player = function() {
+  var plane = document.getElementById("plane");
+  plane.play();
+};
+
+var bulb_player = function() {
+  var bulb = document.getElementById("bulb");
+  bulb.play();
+};
+
+var wheel_player = function() {
+  var wheel = document.getElementById("wheel");
+  wheel.play();
+};
+
 var submitAnswer_Q1 = function() {
     var radios = document.getElementsByName('question1');
     var val= "";
@@ -116,6 +131,41 @@ var submitAnswer_Q3 = function() {
       document.getElementById("QUESTION-VALIDATE-Q3").innerHTML = ""
       document.getElementById("QUESTION-NONE-Q3").innerHTML = "";
       document.getElementById("QUESTION-VALIDATE-WRONG-Q3").innerHTML = "Incorrect! Try again!";
+      var audio = document.getElementById("audio_incorrect");
+      audio.play();
+        }
+
+};
+
+var submitAnswer_Q4 = function() {
+  var radios = document.getElementsByName('question4');
+  var val= "";
+  for (var i = 0, length = radios.length; i < length; i++) {
+      if (radios[i].checked) {
+         val = radios[i].value;
+         break;
+       }
+    }
+  
+    if (val == "Bulb" ) {
+      document.getElementById("QUESTION-VALIDATE-WRONG-Q4").innerHTML = "";
+      document.getElementById("QUESTION-NONE-Q4").innerHTML = "";
+      document.getElementById("QUESTION-VALIDATE-Q4").innerHTML = "Correct! Awesome job!";
+      var audio = document.getElementById("audio_correct");
+      audio.play();
+
+      } 
+    else if (val == "") {
+      document.getElementById("QUESTION-VALIDATE-Q4").innerHTML = "";
+      document.getElementById("QUESTION-VALIDATE-WRONG-Q4").innerHTML = "";
+      document.getElementById("QUESTION-NONE-Q4").innerHTML = "Oops! Please choose an answer!";
+      var audio = document.getElementById("audio_none");
+      audio.play();
+        } 
+    else {
+      document.getElementById("QUESTION-VALIDATE-Q4").innerHTML = ""
+      document.getElementById("QUESTION-NONE-Q4").innerHTML = "";
+      document.getElementById("QUESTION-VALIDATE-WRONG-Q4").innerHTML = "Incorrect! Try again!";
       var audio = document.getElementById("audio_incorrect");
       audio.play();
         }
